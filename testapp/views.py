ee5be233863c5c68ca064a14b4334f4fa1598b24
jithinvijayan007 <jithinvijayan007@gmail.com>
2020-11-02 14:54:38 +0530

    Registration API added

---
+++ testapp/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.generics import UpdateAPIView
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from testapp.serializers import RegistrationSerializer
+from testapp.models import Account
+
+# Create your views here.
+
+@api_view(['POST', ])
+@permission_classes([])
+@authentication_classes([])
+def registration_view(request):
+
+	if request.method == 'POST':
+		data = {}
+		email = request.data.get('email', '0').lower()
+		if validate_email(email) != None:
+			data['error_message'] = 'That email is already in use.'
+			data['response'] = 'Error'
+			return Response(data)
+
+		username = request.data.get('username', '0')
+		if validate_username(username) != None:
+			data['error_message'] = 'That username is already in use.'
+			data['response'] = 'Error'
+			return Response(data)
+
+		serializer = RegistrationSerializer(data=request.data)
+
+		if serializer.is_valid():
+			account = serializer.save()
+			data['response'] = 'successfully registered new user.'
+			data['email'] = account.email
+			data['username'] = account.username
+			data['pk'] = account.pk
+		else:
+			data = serializer.errors
+		return Response(data)
+
+def validate_email(email):
+	account = None
+	try:
+		account = Account.objects.get(email=email)
+	except Account.DoesNotExist:
+		return None
+	if account != None:
+		return email
+
+def validate_username(username):
+	account = None
+	try:
+		account = Account.objects.get(username=username)
+	except Account.DoesNotExist:
+		return None
+	if account != None:
+		return username
